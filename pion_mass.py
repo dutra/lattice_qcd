88fd3ae3c33e8bcac5cab87e24d6b6d6b710cf7c
rht <rudyht@gmail.com>
2012-12-21 16:01:02 -0500

    replace loops with idiomatic scipy

--- pion_mass.py
+++ pion_mass.py
@@ -1,22 +1,25 @@
-
 from pylab import *
+ion()
 
-f = open("data/pion.dat","r")
+#f = open("data/pion.dat","r")
+times, c = loadtxt("data/px0py0pz0_pi_Nsrc174_Ncfg739_32x256_um0p0840_sm0p0743_P.dat",
+        unpack = 1, skiprows=1)
 
-c = zeros((739,256))
+num = len(times)/256
+times = split(times, num)
+c = array(split(c, num))
+#for i in range(4):
+    #figure()
+    ##plot(times[i], log(c[i] / roll(c[i],-1)))
+    #plot(times[i], log(c[i] / c[i+1]))
+    ##plot(times[i], log(c[i]))
 
-for i in arange(0,739) :
-    for j in arange(0,256) :
-        c[i][j] = f.readline().split(" ")[1]
+cmean = c.mean(axis=0)
+cstd = c.std(axis=0) / sqrt(len(c))
 
-m = zeros((739,256))
-for i in arange(0,739):
-    for j in arange(0,255):
-        m[i][j] = log(c[i][j]/c[i][j+1])
+#print shape(cmean)
+#print shape(times[0])
+errorbar(times[0], cmean, cstd)
 
-avg = zeros(256)
-for i in arange(0,256) :
-    avg[i] = sum(m[j][i] for j in arange(0,256))/256
 
-plot(avg)
-show()
+raw_input()
